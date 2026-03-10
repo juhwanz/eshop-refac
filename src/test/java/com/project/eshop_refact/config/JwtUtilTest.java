c3959a9f559bfcf776c5ff4f2309d391ac0f1058
juhwanz <ghdwnghks209@naver.com>
2026-03-10 19:40:30 +0900

    refact : Controller Test

--- src/test/java/com/project/eshop_refact/config/JwtUtilTest.java
+++ src/test/java/com/project/eshop_refact/config/JwtUtilTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Ref;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 // Unit Test : 스프링 컨텍스트 없이 빠르게 시도 가능.
@@ -15,12 +17,12 @@ class JwtUtilTest {
     private JwtUtil jwtUtil;
 
     private static final String TEST_SECRET_KEY = "c2lsdmVybmluZS10ZWNoLXNwcmluZy1ib290LWp3dC10dXRvcmlhbC1zZWNyZXQtMQ=="; // Base64 Encoded
-    private static final long ONE_HOUR = 3600000L;
+    private static final long ONE_HOUR_ExpirationTime = 3600000L;
 
     @BeforeEach
     void setUp() {
-        jwtUtil = new JwtUtil(ONE_HOUR, TEST_SECRET_KEY);
-        jwtUtil.init(); //@PostConstruct 수동 호출.
+        jwtUtil = new JwtUtil(ONE_HOUR_ExpirationTime, TEST_SECRET_KEY); // expirationTime, 키
+        jwtUtil.init(); //@PostConstruct 수동 호출. // 키 암호화.
     }
 
     @Test
@@ -35,7 +37,9 @@ class JwtUtilTest {
 
         //Then
         assertThat(token).isNotBlank();
+        // 토큰 유효성 검증.
         assertThat(jwtUtil.validateToken(token)).isTrue();
+        // 토큰 email.
         assertThat(jwtUtil.getUsernameFromToken(token)).isEqualTo(email);
     }
 
@@ -45,13 +49,14 @@ class JwtUtilTest {
         // Given : 유효시간 0 -> 즉시 만료
         JwtUtil expiredJwtUtil = new JwtUtil(0L, TEST_SECRET_KEY);
         expiredJwtUtil.init();
-        String expiredToken = expiredJwtUtil.createToken("expired@test.com", UserRoleEnum.USER);
+
+        String email = "expired@test.com";
+        UserRoleEnum role = UserRoleEnum.USER;
+        String expiredToken = expiredJwtUtil.createToken(email, role);
 
         //when
-        boolean isValid = expiredJwtUtil.validateToken(expiredToken);
-
         //Then : false여야 함.
-        assertThat(isValid).isFalse();
+        assertThat(expiredJwtUtil.validateToken(expiredToken)).isFalse();
     }
 
     @Test
@@ -61,33 +66,39 @@ class JwtUtilTest {
         String token = jwtUtil.createToken("test@test.com", UserRoleEnum.USER);
 
         // 해커
-        String hackSecretPlain = "hacker-secret-key-must-be-long-enough-for-hmac-sha-algorithms-minimum-bytes";
-        String hackSecretBase64 = java.util.Base64.getEncoder().encodeToString(hackSecretPlain.getBytes());
+        String Hack_secretKeyString = "hacker-secret-key-must-be-long-enough-for-hmac-sha-algorithms-minimum-bytes";
+        String hackSecretBase64 = java.util.Base64.getEncoder().encodeToString(Hack_secretKeyString.getBytes());
 
-        JwtUtil hackJwtUtil = new JwtUtil(3600000L, hackSecretBase64);
+        JwtUtil hackJwtUtil = new JwtUtil(ONE_HOUR_ExpirationTime, hackSecretBase64);
         hackJwtUtil.init();
+
+        String hackerToken = hackJwtUtil.createToken("hacker@test.com", UserRoleEnum.USER);
 
         // When: 해커의 키로 만든 토큰이거나, 내 키로 서명 안 된 토큰 검증
         // (여기서는 상황을 반대로, 내 서버(jwtUtil)에 해커가 만든 토큰을 던졌을 때를 가정)
-        String hackerToken = hackJwtUtil.createToken("hacker@test.com", UserRoleEnum.USER);
-
         //서명이 다르므로 실패해야 함
-        boolean isValid = jwtUtil.validateToken(hackerToken);
-
         // Then
-        assertThat(isValid).isFalse();
+        assertThat(jwtUtil.validateToken(hackerToken)).isFalse();
     }
 
     @Test
     @DisplayName("실패 : 잘못된 형식 토큰")
     void failFormToken(){
         // Given
-        String garbageToken = "Bearer invalid.token.structure"; // 중간 띄어쓰기 밑 Bearer 그대로 넣음 -> 형식 에러 [MalformedJwtException]
-
-        // when
-        boolean isValid = jwtUtil.validateToken(garbageToken);
+        String garbageToken = "Bearer invalid.token.structure"; // 중간 띄어쓰기 밑 Bearer 그대로 넣음 -> 형식 에러 [MalformedJwtException)
 
         //Then
-        assertThat(isValid).isFalse();
+        assertThat(jwtUtil.validateToken(garbageToken)).isFalse();
+    }
+
+    @Test
+    @DisplayName("성공 : Refresh Token 생성 및 검증")
+    void createValidRefreshToken(){
+        //Given
+        String RefreshToken = jwtUtil.createRefreshToken("test@test.com");
+
+        assertThat(RefreshToken).isNotBlank();
+        assertThat(jwtUtil.validateToken(RefreshToken)).isTrue();
+        assertThat(jwtUtil.getUsernameFromToken(RefreshToken)).isEqualTo("test@test.com");
     }
 }
--- src/test/java/com/project/eshop_refact/controller/ProductControllerTest.java
+++ src/test/java/com/project/eshop_refact/controller/ProductControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -32,17 +33,23 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 // 대기열 인터셉터 문제 발생 X -> WebConfig에서 /api/orders만 적용했기에.
+// SecurityConfig는 배제.
 @WebMvcTest(controllers = ProductController.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class))
 class ProductControllerTest {
 
+    // [가짜 HTTP 요청 도구] : WAS 띄우지 않고, GET,POST같은 요청을 흉내내주는 스프링 테스트 객체
     @Autowired MockMvc mockMvc;
+    // [JSON 변환 도구 ] : DTO -> JSON 직렬화 , JSON -> DTO 역직렬화 역할.
     @Autowired ObjectMapper objectMapper;
+    // [ Mock 주입]
     @MockBean ProductService productService;
 
-    // 요 2놈 떄문에 에러 자꾸 뜸.
+    // [컨텍스트 로드 에러 방지용 껍데기]
+    // 요 2놈 떄문에 에러 자꾸 뜸. -> 웹 관련 Bean들을 띄울 때, 기본적으로 필터 체인, 다른 인터셉트 동작
+    // -> 빈 껍데기라도 던져줘야 함. ????
     @MockBean
     JwtUtil jwtUtil;
     @MockBean
@@ -51,50 +58,131 @@ class ProductControllerTest {
     com.project.eshop_refact.service.queue.WaitingQueueService waitingQueueService;
 
     @Test
-    @DisplayName("상품 등록 API - 201 Created 반환 검증") // 테스트 이름도 명확하게!
+    @DisplayName("상품 등록 API - 201 Created 반환 검증")
+    // [가짜 사용자] : SecurityConfig를 껏지만, 여전히 메서드 보안 권한 필요 할 수도 -> 가짜.
     @WithMockUser
     void registerProduct() throws Exception {
         // given
+        // @RequsetBody를 통과하기 위함.
         ProductDto.RegisterRequest request = new ProductDto.RegisterRequest();
         request.setName("New Item");
         request.setPrice(1000);
         request.setStockQuantity(100);
 
+        // [Stubbing (가짜 객체 행동 정의)] : Mockito 라이브러리 기능
+        // -> 아무 값이나 들어와도 1L 반환. : service 우회법
         given(productService.registerProduct(any())).willReturn(1L);
 
         // when & then
         mockMvc.perform(post("/api/products")
+                        // 스프링 시큐리티 기본 요구 : CSRF 해킹 방어 토큰 => 403 에러 발생.
                         .with(csrf())
+                        .characterEncoding("UTF-8")
+                        // JSON 형식이야 - 헤더
                         .contentType(MediaType.APPLICATION_JSON)
+                        // 직렬화 - 바디
+
                         .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isCreated())
-                .andExpect(content().string("상품등록 성공"))
+                .andExpect(jsonPath("$.message").value("상품등록 성공"))
+                .andExpect(jsonPath("$.data").value(1L))
                 .andDo(print());
     }
 
+    @Test
+    @DisplayName("단일 상품 조회 API")
+    @WithMockUser
+    void getProductById() throws Exception{
+        //Given
+        Long productId = 1L;
+        Product product = new Product("MAC", 250000, 5);
+        ReflectionTestUtils.setField(product, "id", productId);
+        ProductDto.Response responseDto = new ProductDto.Response(product);
+
+        //PathVariable로 넘어온 productId를 -> 서비스로 넘김
+        given(productService.getProductById(productId)).willReturn(responseDto);
+
+        //When & Then
+        mockMvc.perform(get("/api/products/{productId}",productId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("MAC"))
+                .andExpect(jsonPath("$.price").value(250000))
+                .andDo(print());
+    }
     @Test
     @DisplayName("상품 검색 API")
     @WithMockUser
     void searchProducts() throws Exception {
         // given
         Product product = new Product("MacBook", 2000000, 10);
-        ReflectionTestUtils.setField(product, "id", 100L); // ID 주입
+        ReflectionTestUtils.setField(product, "id", 100L); // id = 100L
 
         ProductDto.Response responseDto = new ProductDto.Response(product);
         Page<ProductDto.Response> pageResponse = new PageImpl<>(List.of(responseDto));
 
-        // Controller는 파라미터를 객체(SearchCondition)로 받으므로 any() 사용
+        //[Stubbing] : /search 호출 시 pageResponse를 리턴해라.
         given(productService.search(any(), any())).willReturn(pageResponse);
 
         // when & then
+        // get 요청 시에는 csrf 방어 필요 X -> .with(csrf()) 생략.
         mockMvc.perform(get("/api/products/search")
+                        // url 뒤 파라미터 (?name=MacBook&minPrice=100000)
                         .param("name", "MacBook")
                         .param("minPrice", "1000000")
                         .param("page", "0")
                         .param("size", "10"))
-                .andExpect(status().isOk())
+                .andExpect(status().isOk()) // 200??
+                // & - JSON의 최상위 루트
                 .andExpect(jsonPath("$.content[0].name").value("MacBook"))
                 .andExpect(jsonPath("$.content[0].price").value(2000000))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("상품 검색 API (No-offset/Slice)")
+    @WithMockUser
+    void searchNoOffset()throws Exception{
+        //Given
+        Product product = new Product("Phone",20000,20);
+        ReflectionTestUtils.setField(product, "id", 50L);
+        ProductDto.Response responseDto = new ProductDto.Response(product);
+
+        //Slice
+        Slice<ProductDto.Response> slice = new org.springframework.data.domain.SliceImpl<>(List.of(responseDto));
+
+        //any
+        given(productService.searchNoOffset(any(),any(), any())).willReturn(slice);
+
+        mockMvc.perform(get("/api/products/search/no-offset")
+                    .param("lastProductId", "49")
+                    .param("name", "Phone")
+                    .param("size", "20"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].name").value("Phone"))
+                .andExpect(jsonPath("$.content[0].price").value(20000))
+                .andDo(print());
+    }
+    @Test
+    @DisplayName("상품 가격 수정 API (PatchMapping)")
+    @WithMockUser
+    void updateProductPrice() throws Exception {
+        // given
+        Long productId = 1L;
+        int newPrice = 2800000;
+        Product product = new Product("MacBook Pro", newPrice, 5); // 가격이 수정된 상태 가정
+        ReflectionTestUtils.setField(product, "id", productId);
+        ProductDto.Response responseDto = new ProductDto.Response(product);
+
+        given(productService.updateProductPrice(productId, newPrice)).willReturn(responseDto);
+
+        // when & then
+        // PATCH 요청, 데이터 변경이 일어나므로 POST와 마찬가지로 CSRF 토큰 필요
+        mockMvc.perform(patch("/api/products/{productId}/price", productId)
+                        .with(csrf())
+                        .param("newPrice", String.valueOf(newPrice))) // @RequestParam 매핑
+                .andExpect(status().isOk())
+                // ApiResponse 객체 구조(message, data)에 맞춰 jsonPath 검증
+                .andExpect(jsonPath("$.message").value("가격수정 성공"))
+                .andExpect(jsonPath("$.data.price").value(newPrice))
                 .andDo(print());
     }
 }
