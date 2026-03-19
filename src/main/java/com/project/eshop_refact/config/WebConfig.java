6ca3938079d56a4759645dc9001c8d47751f4f12
juhwanz <ghdwnghks209@naver.com>
2026-03-19 21:14:23 +0900

    refact : Test Code

--- src/main/java/com/project/eshop_refact/config/WebConfig.java
+++ src/main/java/com/project/eshop_refact/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     // Traffic Throttling: 대규모 트래픽 유입 시 시스템 과부하 방지를 위한 유량 제어(Flow Control) 적용
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(queueInterceptor)
+       registry.addInterceptor(queueInterceptor)
                 .addPathPatterns("/api/orders/**") // 하위 API 확장 대비
                 .excludePathPatterns("/api/orders/queue"); // 대기열 확인 API 제외
     }
